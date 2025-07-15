f45846444af28c3860463f44b2c606f69c92125f
aishika7 <aishikamajumdar88@gmail.com>
2025-07-16 02:00:42 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://aishika07:Mong_aishi_07@meanproject.hhuao.mongodb.net/e-learning-db');
+    await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://YOUR_MDB_URL');
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err.message);
